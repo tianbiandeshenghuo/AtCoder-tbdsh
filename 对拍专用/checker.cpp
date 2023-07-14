be43f9fd5e97483e0dd514cee1683427efa12a97
天边的生活 <104612354+tianbiandeshenghuo@users.noreply.github.com>
2023-07-14 07:33:35 +0000

    init commit

---
+++ 对拍专用/checker.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define rd(x) rd() % x
+
+using namespace std;
+const long long MAXN = 100;
+int main(){
+  ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  freopen("t.in", "w", stdout);
+  random_device rd;
+  cout << "1\n";
+  int n = rd(10) + 1;
+  cout << n << '\n';
+  for (int i = 1; i <= n; i++){
+    cout << rd(n) << ' ';
+  }
+  return 0;
+}
