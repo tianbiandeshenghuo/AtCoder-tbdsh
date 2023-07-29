c6c009e34ba323d91504f05d7137e3feebe10492
天边的生活 <104612354+tianbiandeshenghuo@users.noreply.github.com>
2023-07-29 13:46:01 +0000

    init commit

--- test.cpp
+++ test.cpp
@@ -1,145 +1,98 @@
-#include<cstdio>
-#include<cstdlib>
-#include<ctime>
-const int N=2e5+10;
-struct fhq_treap
-{
-    struct node
-    {
-        int l,r;
-        int val;
-        int key;
-        int size;
-    }t[N];
-    int tot,root;
-    #define ls t[k].l
-    #define rs t[k].r
-    int newnode(int val)
-    {
-        t[++tot].val=val;
-        t[tot].key=rand()%114514191;
-        t[tot].size=1;
-        return tot;
+#include<bits/stdc++.h>
+#pragma GCC optimize(3)
+#pragma GCC target("avx")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("inline")
+#pragma GCC optimize("-fgcse")
+#pragma GCC optimize("-fgcse-lm")
+#pragma GCC optimize("-fipa-sra")
+#pragma GCC optimize("-ftree-pre")
+#pragma GCC optimize("-ftree-vrp")
+#pragma GCC optimize("-fpeephole2")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-fsched-spec")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("-falign-jumps")
+#pragma GCC optimize("-falign-loops")
+#pragma GCC optimize("-falign-labels")
+#pragma GCC optimize("-fdevirtualize")
+#pragma GCC optimize("-fcaller-saves")
+#pragma GCC optimize("-fcrossjumping")
+#pragma GCC optimize("-fthread-jumps")
+#pragma GCC optimize("-funroll-loops")
+#pragma GCC optimize("-fwhole-program")
+#pragma GCC optimize("-freorder-blocks")
+#pragma GCC optimize("-fschedule-insns")
+#pragma GCC optimize("inline-functions")
+#pragma GCC optimize("-ftree-tail-merge")
+#pragma GCC optimize("-fschedule-insns2")
+#pragma GCC optimize("-fstrict-aliasing")
+#pragma GCC optimize("-fstrict-overflow")
+#pragma GCC optimize("-falign-functions")
+#pragma GCC optimize("-fcse-skip-blocks")
+#pragma GCC optimize("-fcse-follow-jumps")
+#pragma GCC optimize("-fsched-interblock")
+#pragma GCC optimize("-fpartial-inlining")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("-freorder-functions")
+#pragma GCC optimize("-findirect-inlining")
+#pragma GCC optimize("-fhoist-adjacent-loads")
+#pragma GCC optimize("-frerun-cse-after-loop")
+#pragma GCC optimize("inline-small-functions")
+#pragma GCC optimize("-finline-small-functions")
+#pragma GCC optimize("-ftree-switch-conversion")
+#pragma GCC optimize("-foptimize-sibling-calls")
+#pragma GCC optimize("-fexpensive-optimizations")
+#pragma GCC optimize("-funsafe-loop-optimizations")
+#pragma GCC optimize("inline-functions-called-once")
+#pragma GCC optimize("-fdelete-null-pointer-checks")
+#pragma GCC optimize(2)
+#pragma GCC optimize(1)
+
+using namespace std;
+const int MAXN = 1e6 + 5;
+int n, m;
+struct Node{
+  int x, id;
+} a[MAXN];
+vector<int> b[25];
+int main(){
+  ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  freopen("contest.in", "r", stdin);
+  freopen("contest.out", "w", stdout);
+  cin >> n >> m;
+  for (int i = 1; i <= n; i++){
+    cin >> a[i].x;
+    a[i].id = i;
+  }
+  int q = log2(n);
+  q--;
+  for (int i = 1; i < n; i += 2){
+    if (a[i + 1].x > a[i].x){
+      b[q].push_back(a[i + 1].id);
+    }else {
+      b[q].push_back(a[i].id);
     }
-    void pushup(int k)
-    {
-        t[k].size=t[ls].size+t[rs].size+1;
+  }
+  n /= 2;
+  for (int i = q - 1; n; i--, n /= 2){
+    for (int j = 0; j < n - 1; j += 2){
+      if (a[b[i + 1][j + 1]].x > a[b[i + 1][j]].x){
+        b[i].push_back(a[b[i + 1][j + 1]].id);
+      }else {
+        b[i].push_back(a[b[i + 1][j]].id);
+      }
     }
-    void split(int k,int val,int &x,int &y)
-    {
-        if(!k) x=y=0;
-        else
-        {
-            if(t[k].val<=val)
-            {
-                x=k;
-                split(rs,val,rs,y);
-            }
-            else
-            {
-                y=k;
-                split(ls,val,x,ls);
-            }
-            pushup(k);
-        }
+  }
+  while (m--){
+    int x;
+    cin >> x;
+    x = log2(x);
+    //cerr << x << '\n';
+    for (auto v : b[x]){
+      cout << v << ' ';
     }
-    int merge(int x,int y)
-    {
-        if(!x||!y) return x+y;
-        if(t[x].key>t[y].key)
-        {
-            t[x].r=merge(t[x].r,y);
-            pushup(x);
-            return x;
-        }
-        else
-        {
-            t[y].l=merge(x,t[y].l);
-            pushup(y);
-            return y;
-        }
-    }
-    void add(int val)
-    {
-        int x,y;
-        split(root,val,x,y);
-        root=merge(merge(x,newnode(val)),y);
-    }
-    void del(int val)
-    {
-        int x,y,z;
-        split(root,val,x,z);
-        split(x,val-1,x,y);
-        y=merge(t[y].l,t[y].r);
-        root=merge(merge(x,y),z);
-    }
-    int getrank(int val)
-    {
-        int x,y;
-        split(root,val-1,x,y);
-        int res=t[x].size+1;
-        root=merge(x,y);
-        return res;
-    }
-    int getnum(int rank)
-    {
-        int k=root;
-        while(k)
-        {
-            if(t[ls].size+1==rank) break;
-            else if(t[ls].size>=rank) k=t[k].l;
-            else
-            {
-                rank-=t[ls].size+1;
-                k=rs;
-            }
-        }
-        return t[k].val;
-    }
-    int pre(int val)
-    {
-        int x,y;
-        split(root,val-1,x,y);
-        int k=x;
-        while(rs)
-            k=rs;
-        int res=t[k].val;
-        root=merge(x,y);
-        return res;
-    }
-    int nxt(int val)
-    {
-        int x,y;
-        split(root,val,x,y);
-        int k=y;
-        while(ls)
-            k=ls;
-        int res=t[k].val;
-        root=merge(x,y);
-        return res;
-    }
-}tree;
-int a[N];
-int ans[N];
-int main()
-{
-    int t;
-    scanf("%d",&t);
-    while(t--)
-    {
-        int n;
-        scanf("%d",&n);
-        for(int i=1;i<=n;i++)
-        {
-            tree.add(i);
-            scanf("%d",&a[i]);
-        }
-        for(int i=n;i>=1;i--)
-        {
-            ans[i]=tree.getnum(i-a[i]);
-            tree.del(ans[i]);
-        }
-        for(int i=1;i<=n;i++) printf("%d ",ans[i]);
-        printf("\n");
-    }
+    cout << '\n';
+  }
+  return 0;
 }
