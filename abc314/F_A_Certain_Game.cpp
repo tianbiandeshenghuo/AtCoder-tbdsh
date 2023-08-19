9088292130d2d4b23597d15ccc42c044148268b6
天边的生活 <104612354+tianbiandeshenghuo@users.noreply.github.com>
2023-08-19 14:42:17 +0000

    update abc315

--- abc314/F_A_Certain_Game.cpp
+++ abc314/F_A_Certain_Game.cpp
@@ -1,11 +1,12 @@
 #include<bits/stdc++.h>
+#define int long long
 
 using namespace std;
 const int MAXN = 2e5 + 5, Mod = 998244353;
 int n, c[MAXN], sum[MAXN], qp[MAXN];
 long long inv[2 * MAXN];
 vector<int> b[MAXN];
-int main(){
+signed main(){
   ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
   cin >> n;
   for (int i = 1; i <= n; i++){
